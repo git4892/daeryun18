98f840b12841adc3026541938dc0bd8ff7cdd5d0
git4892 <type4892@gmail.com>
2018-09-10 15:34:17 +0900

    Add: python for loop ex
    for loop ex (1to10 sum),(배수)

---
+++ 10315이민기-1to10합.py
@@ -0,0 +1,6 @@
+print("1부터 10까지의 합을 구하는 프로그램")
+sum = 1;
+for i in range(10-1):
+    print (sum,'+',i+2,'=', sum+i+2)
+    sum = sum+i+2
+    pass
---
+++ 10315이민기-배수.py
@@ -0,0 +1,4 @@
+print("1부터 100까지 3과 7의 공통 배수 구하기")
+for i in range(100):
+    if (i%3==0) and (i%7==0):
+        print(i)
